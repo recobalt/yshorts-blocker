2af6d62a0b9e69751a7ad04a0b46345bac4f0dc8
Egor <113925456+recobalt@users.noreply.github.com>
2025-09-22 21:36:35 +0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,50 @@
+const styleTag = document.createElement("style");
+const styleContents = document.createTextNode(`
+ytd-mini-guide-entry-renderer[aria-label="Shorts"],
+ytd-rich-section-renderer,
+ytd-reel-shelf-renderer ,
+[title="Shorts"]  {
+     display: none; 
+    }
+`);
+styleTag.appendChild(styleContents);
+document.body.prepend(styleTag);
+
+function removeUIElements() {
+  // Remove the Shorts button from the sidebar
+  const shortsButton = document.querySelector(
+    'ytd-mini-guide-entry-renderer[aria-label="Shorts"]'
+  );
+  if (shortsButton) {
+    shortsButton.remove();
+  }
+
+  const carousels = document.querySelectorAll(
+    "ytd-rich-section-renderer, ytd-reel-shelf-renderer"
+  );
+  carousels.forEach((carousel) => {
+    // Add specific condition to target Shorts carousel if needed
+    carousel.remove();
+  });
+
+  const otherShortsButtons = document.querySelectorAll('[title="Shorts"]');
+  otherShortsButtons.forEach((node) => node.remove());
+}
+
+removeUIElements();
+
+const observer = new MutationObserver((mutations) => {
+  let shouldRemoveElements = false;
+  for (const mutation of mutations) {
+    if (mutation.addedNodes.length > 0) {
+      shouldRemoveElements = true;
+      break;
+    }
+  }
+
+  if (shouldRemoveElements) {
+    removeUIElements();
+  }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
